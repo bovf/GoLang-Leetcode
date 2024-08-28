3b9409edfdeca22623c8a951fc72b1c8e50b6fcd
bovfbovf <dobry989@gmail.com>
2024-08-28 09:46:41 +0300

    Daily commit

---
+++ 11.ContainerWithMostWater.go
@@ -0,0 +1,35 @@
+package golangleetcode
+
+// Very slow and bruteforce
+// func maxArea(height []int) int {
+// 	result := 0
+// 	for i := 0; i < len(height); i++ {
+// 		for x := i + 1; x < len(height); x++ {
+// 			area := min(height[i], height[x]) * (x - i)
+// 			if area > result {
+// 				result = area
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
+func maxArea(height []int) int {
+	left, right := 0, len(height)-1
+	result := 0
+	for left < right {
+		width := right - left
+		area := min(height[left], height[right]) * width
+
+		if area > result {
+			result = area
+		}
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return result
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bovf/GoLang-Leetcode
+
+go 1.21.1

8260615fa05ec3d02c5247dc35906163b5d4b85f
bovfbovf <dobry989@gmail.com>
2024-09-03 09:40:14 +0300

    Daily commit

--- 16.3SumClosest.go
+++ 16.3SumClosest.go
@@ -5,31 +5,58 @@ import (
 	"sort"
 )
 
+// func threeSumClosest(nums []int, target int) int {
+// 	sort.Ints(nums)
+// 	result := nums[0] + nums[1] + nums[2]
+//
+// 	for i := 0; i < len(nums)-2; i++ {
+//
+// 		left, right := i+1, len(nums)-1
+// 		for left < right {
+// 			sum := nums[i] + nums[left] + nums[right]
+// 			distance := sum - target
+// 			old_distance := result - target
+//
+// 			if math.Abs(float64(distance)) < math.Abs(float64(old_distance)) {
+// 				result = sum
+// 			}
+//
+// 			if sum < target {
+// 				left++
+// 			} else if sum > target {
+// 				right--
+// 			} else {
+// 				return sum
+// 			}
+// 		}
+// 	}
+//
+// 	return result
+// }
+
+// Faster
 func threeSumClosest(nums []int, target int) int {
 	sort.Ints(nums)
-	result := nums[0] + nums[1] + nums[2]
+	closestSum := nums[0] + nums[1] + nums[2]
 
 	for i := 0; i < len(nums)-2; i++ {
 
 		left, right := i+1, len(nums)-1
 		for left < right {
-			sum := nums[i] + nums[left] + nums[right]
-			distance := sum - target
-			old_distance := result - target
+			currentSum := nums[i] + nums[left] + nums[right]
 
-			if math.Abs(float64(distance)) < math.Abs(float64(old_distance)) {
-				result = sum
-			}
-
-			if sum < target {
+			if currentSum == target {
+				return target
+			} else if currentSum < target {
 				left++
-			} else if sum > target {
-				right--
 			} else {
-				return sum
+				right--
+			}
+			if math.Abs(float64(currentSum-target)) < math.Abs(float64(closestSum-target)) {
+				closestSum = currentSum
 			}
 		}
 	}
 
-	return result
+	return closestSum
 }

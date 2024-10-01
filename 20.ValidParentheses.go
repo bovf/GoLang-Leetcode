e21408a67c7e1b4ae38bfcaae88dcdc1c82e3317
bovfbovf <dobry989@gmail.com>
2024-10-01 16:55:48 +0300

    Daily commit

--- 20.ValidParentheses.go
+++ 20.ValidParentheses.go
@@ -3,12 +3,35 @@ package main
 import "fmt"
 
 func isValid(s string) bool {
-	result := false
+	var result bool
 
 	if len(s)%2 != 0 {
+		result = false
 		return result
 	}
 
+	stack := []rune{}
+	openBrackets := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+
+	for _, r := range s {
+		if closer, ok := openBrackets[r]; ok {
+			stack = append(stack, closer)
+			continue
+		}
+
+		stackLength := len(stack)
+		if stackLength == 0 || r != stack[stackLength-1] {
+			result = false
+			return result
+		}
+
+		stack = stack[:stackLength-1]
+	}
+	result = true
 	return result
 }
 
